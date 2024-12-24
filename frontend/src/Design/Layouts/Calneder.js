f7a22f9348ce75e9e21f3c1559e7ceb9876fea35
Darshana Perera <ds.perera.1997@gmail.com>
2024-12-24 21:21:51 +0530

    calender added

--- frontend/src/Design/Layouts/Calneder.js
+++ frontend/src/Design/Layouts/Calneder.js
@@ -1,21 +1,23 @@
 import React from "react";
-import tree01 from "../Assets/tree01.png";
+import tree01 from "../Assets/Calender-2025.jpg";
 
 const Calneder = () => {
   return (
     <div className="calneder-banner">
       <div className="container py-5 my-5">
-        <div className="row align-items-center ">
+        <div className="row align-items-center">
           {/* Text and Form Section */}
           <div className="col-md-6 text-center text-md-start">
             <h1 className="txt1">Download</h1>
-            <h1 className="mb-3 txt2">2025 Calender</h1>
+            <h1 className="mb-3 txt2">2025 Calendar</h1>
             <h2 className="mb-3">As a soft-copy</h2>
 
             {/* Button Section */}
-            <button className="btn btn-primary custom-btn2">
-              Download Now
-            </button>
+            <a href={tree01} download>
+              <button className="btn btn-primary custom-btn2">
+                Download Now
+              </button>
+            </a>
           </div>
         </div>
       </div>
